74330e78723cd63c4af5073038f4595acf44eb7f
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-23 16:21:57 -0400

    Updates

---
+++ precache-manifest.7a74da16fed2e5d10d50d8c3441cd2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7942d9ae03ca9811700a8827986ecbe",
+    "url": "/reactShoppingCart/index.html"
+  },
+  {
+    "revision": "49ff623dd51b25080069",
+    "url": "/reactShoppingCart/static/css/main.8db4b5e8.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/reactShoppingCart/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactShoppingCart/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ff623dd51b25080069",
+    "url": "/reactShoppingCart/static/js/main.5f3978ff.chunk.js"
+  },
+  {
+    "revision": "7905155448dc7f31235a",
+    "url": "/reactShoppingCart/static/js/runtime-main.c43b0158.js"
+  }
+]);
